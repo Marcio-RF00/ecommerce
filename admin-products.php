165e4b89e7e863c8d5c41a0908d168fc1f8b111b
Marcio-RF00 <marciuflad@gmail.com>
2017-11-17 22:50:38 -0200

    Admin - Products - Search e Pagination

--- admin-products.php
+++ admin-products.php
@@ -1,21 +1,51 @@
-<?php  
+<?php 
 
+use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Product;
-use \Hcode\PageAdmin;
-
 
 $app->get("/admin/products", function(){
 
 	User::verifyLogin();
 
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if ($search != '') {
+
+		$pagination = Product::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Product::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/products?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
+
 	$products = Product::listAll();
 
 	$page = new PageAdmin();
 
-	$page -> setTpl("products", [
-		"products"=>$products
+	$page->setTpl("products", [
+		"products"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
 	]);
+
 });
 
 $app->get("/admin/products/create", function(){
@@ -24,7 +54,8 @@ $app->get("/admin/products/create", function(){
 
 	$page = new PageAdmin();
 
-	$page -> setTpl("products-create");
+	$page->setTpl("products-create");
+
 });
 
 $app->post("/admin/products/create", function(){
@@ -33,12 +64,13 @@ $app->post("/admin/products/create", function(){
 
 	$product = new Product();
 
-	$product -> setData($_POST);
+	$product->setData($_POST);
 
 	$product->save();
 
 	header("Location: /admin/products");
 	exit;
+
 });
 
 $app->get("/admin/products/:idproduct", function($idproduct){
@@ -47,13 +79,14 @@ $app->get("/admin/products/:idproduct", function($idproduct){
 
 	$product = new Product();
 
-	$product -> get((int)$idproduct);
+	$product->get((int)$idproduct);
 
 	$page = new PageAdmin();
 
-	$page -> setTpl("products-update", [
-		"product"=>$product->getValues()
+	$page->setTpl("products-update", [
+		'product'=>$product->getValues()
 	]);
+
 });
 
 $app->post("/admin/products/:idproduct", function($idproduct){
@@ -62,16 +95,17 @@ $app->post("/admin/products/:idproduct", function($idproduct){
 
 	$product = new Product();
 
-	$product -> get((int)$idproduct);
+	$product->get((int)$idproduct);
 
-	$product -> setData($_POST);
+	$product->setData($_POST);
 
 	$product->save();
 
 	$product->setPhoto($_FILES["file"]);
 
-	header("Location: /admin/products");
+	header('Location: /admin/products');
 	exit;
+
 });
 
 $app->get("/admin/products/:idproduct/delete", function($idproduct){
@@ -80,11 +114,13 @@ $app->get("/admin/products/:idproduct/delete", function($idproduct){
 
 	$product = new Product();
 
-	$product -> get((int)$idproduct);
+	$product->get((int)$idproduct);
 
 	$product->delete();
 
-	header("Location: /admin/products");
+	header('Location: /admin/products');
 	exit;
+
 });
-?>
+
+ ?>
